bfc95a19c6f9d3f6509c0eee7e3abe64297610e7
yezhisaim <yezhisaim@gmail.com>
2015-07-11 15:47:48 -0700

    Additional view controllers for the watch
    Adding watch kit view and notification controllers

---
+++ iosDevCampApp/iosDevCampApp WatchKit App/WKNewLocationNotificationController.swift
@@ -0,0 +1,54 @@
+//
+//  WKNewLocationNotificationController.swift
+//  iosDevCampApp
+//
+//  Created by Yezhisai Murugesan on 7/11/15.
+//  Copyright (c) 2015 Isai. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+//New Location Notification Controller.
+
+class WKNewLocationNotificationController: WKUserNotificationInterfaceController {
+
+    override init() {
+        // Initialize variables here.
+        super.init()
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    /*
+    override func didReceiveLocalNotification(localNotification: UILocalNotification, withCompletion completionHandler: ((WKUserNotificationInterfaceType) -> Void)) {
+        // This method is called when a local notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+        //
+        // After populating your dynamic notification interface call the completion block.
+        completionHandler(.Custom)
+    }
+    */
+    
+    /*
+    override func didReceiveRemoteNotification(remoteNotification: [NSObject : AnyObject], withCompletion completionHandler: ((WKUserNotificationInterfaceType) -> Void)) {
+        // This method is called when a remote notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+        //
+        // After populating your dynamic notification interface call the completion block.
+        completionHandler(.Custom)
+    }
+    */
+}
---
+++ iosDevCampApp/iosDevCampApp WatchKit App/WKViewController.swift
@@ -0,0 +1,32 @@
+//
+//  WKViewController.swift
+//  iosDevCampApp
+//
+//  Created by Yezhisai Murugesan on 7/11/15.
+//  Copyright (c) 2015 Isai. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+//This is the main view controller on the watch.
+
+class WKViewController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
